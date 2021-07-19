61c50baf2e0ea75d5c2d7b93794baff7a08b90a9
ArihantJain123 <arihantjain2708@gmail.com>
2021-07-19 14:44:27 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+    return (
+            <>
+
+            <div className="text-center new">
+             <p className= "let">Lets Grow More</p>   
+            <p>Copyright &copy; ReactApp</p>
+            </div>
+       
+             </>
+    )
+}
